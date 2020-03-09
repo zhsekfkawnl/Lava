eccd36e5176164968680b358745a643f21fd867d
Class02 <Class02@DESKTOP-JCUK6AH>
2020-03-09 15:10:26 +0900

    RAVA

---
+++ Java00/src/day01/Test01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Test01 {
+    public static void main(String[] args) {
+    	System.out.println("Hello Lava!!!");
+    	// ==> println() 함수(method) - 콘솔창에 내용을 출력해주는 기능가진 함수
+    	// 이클립스에서 자바클래스 실행하기
+    	// 컨트롤키 + 쉬프트키 + F11
+    }
+}
